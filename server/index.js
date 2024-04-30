37a29a8a22c0e18c263df32c2ca83b481467064f
Xendorr <79709918+Xendorr@users.noreply.github.com>
2024-04-30 20:41:36 +0100

    removed cors -> handled by nginx.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 import authRoute from "./Routes/AuthRoute.js";
 import movieRoute from "./Routes/MovieRoute.js";
 import userRoute from "./Routes/UserRoute.js";
@@ -14,14 +12,7 @@ const app = express();
 
 const PORT = process.env.PORT || 8081;
 
-// const corsOptions = {
-//   origin: "http://localhost:5173",
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   credentials: true,
-// };
-
 app.use(cookieParser());
-app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
